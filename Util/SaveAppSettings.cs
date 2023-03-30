4479c64f8ff341d2ca6085a42af74de385f17584
HaRisH123KUmAr <68292739+HaRisH123KUmAr@users.noreply.github.com>
2023-03-30 15:56:26 +0530

    Changed namespaces

---
+++ Util/SaveAppSettings.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.UI.Composition;
+
+namespace UWPYourNoteLibrary.Util
+{
+    public class SaveAppSettings
+    {
+        public static void SavePreferences(string theme)
+        {
+            ApplicationDataContainer localSettings = Windows.Storage.ApplicationData.Current.LocalSettings;
+
+            // Save a setting locally on the device
+            localSettings.Values["ApplicationTheme"] = theme;
+
+        }
+        public static string LoadPreferences()
+        {
+            ApplicationDataContainer localSettings = Windows.Storage.ApplicationData.Current.LocalSettings;
+
+            // load a setting that is local to the device
+            String localValue = localSettings.Values["ApplicationTheme"] as string;
+            return localValue;
+
+        }
+    }
+}
